78275849288c006a158d667fe9e50cb9bace6316
bimmyfox <kolgatinadasha@gmail.com>
2019-01-21 16:45:10 +0300

    add stable mobile and unity editor input

--- Assets/Scripts/InputControll.cs
+++ Assets/Scripts/InputControll.cs
@@ -1,49 +1,92 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 namespace Game
 {
-    public class InputControll : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+    public class InputControll : MonoBehaviour
     {
-        bool swipe = false;
+        Vector3 firstTouchPosition;   //Первая позиция касания
+        Vector3 lastTouchPosition;    //Последняя позиция касания
+        [SerializeField] float dragDistance = 10f;  //Минимальная дистанция для определения свайпа
 
-        public void OnBeginDrag(PointerEventData eventData)
-        {
-            swipe = true;
-            if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
-            {
-                if (eventData.delta.x > 0)
-                {
-                    Main.self.Player.State = PlayerFSM.SwipeRight;
-                }
-                else
-                {
-                    Main.self.Player.State = PlayerFSM.SwipeLeft;
-                }
-            }
-        }
+        List<Vector3> touchPositions = new List<Vector3>();
 
-        public void OnEndDrag(PointerEventData eventData)
+        void Start()
         {
-            swipe = false;
+            dragDistance *= Screen.height / 100;
         }
 
         void Update()
         {
-            if (Input.touchCount == 2)
-            {
-                Main.self.Player.State = PlayerFSM.Jump;
-            }
+            MobileInput();
+
 
 #if UNITY_EDITOR
-            if ((Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) && !swipe)
-            {
-                Main.self.Player.State = PlayerFSM.Jump;
-            }
+            UnityEditorInput();
 #endif
         }
 
-        public void OnDrag(PointerEventData eventData) { }
+        void UnityEditorInput()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Main.self.Player.State = PlayerFSM.Jump;
+            }
+            if (Input.GetKeyDown("left"))
+            {
+                Main.self.Player.State = PlayerFSM.SwipeLeft;
+            }
+            if (Input.GetKeyDown("right"))
+            {
+                Main.self.Player.State = PlayerFSM.SwipeRight;
+            }
+        }
 
+
+        void MobileInput()
+        {
+            if (Input.touchCount <= 0)
+            { return; }
+            if (Input.touches[0].phase == TouchPhase.Moved)
+            {
+                touchPositions.Add(Input.touches[0].position);
+            }
+
+            //Касание
+            if (Input.touches[0].phase == TouchPhase.Ended && touchPositions.Count == 0)
+            {
+                Main.self.Player.State = PlayerFSM.Jump;
+            }
+
+            //Свайп
+            if (Input.touches[0].phase == TouchPhase.Ended && touchPositions.Count > 0)
+            {
+                firstTouchPosition = touchPositions[0];
+                lastTouchPosition = touchPositions[touchPositions.Count - 1];
+
+
+                if (Mathf.Abs(lastTouchPosition.x - firstTouchPosition.x) > Mathf.Abs(lastTouchPosition.y - firstTouchPosition.y))
+                {
+                    //Если дистанция свайпа коротка
+                    if (Mathf.Abs(lastTouchPosition.x - firstTouchPosition.x) < dragDistance)
+                    {
+                        return;
+                    }
+
+                    if (lastTouchPosition.x < firstTouchPosition.x)
+                    {
+                        //Свайп влево
+                        Main.self.Player.State = PlayerFSM.SwipeLeft;
+                    }
+
+                    if (lastTouchPosition.x > firstTouchPosition.x)
+                    {
+                        //Свайп вправо
+                        Main.self.Player.State = PlayerFSM.SwipeRight;
+                    }
+                }
+                touchPositions.Clear();
+            }
+        }
     }
 }
