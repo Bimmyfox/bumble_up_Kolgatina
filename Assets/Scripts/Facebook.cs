e5ec1504ae55a7ef6b58c8aa4fa80a976b4385fb
bimmyfox <kolgatinadasha@gmail.com>
2019-01-23 01:13:30 +0300

    import facebook sdk, add base loginFb script

---
+++ Assets/Scripts/Facebook.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using Facebook.Unity;
+using System.Collections.Generic;
+
+namespace Facebook
+{
+    public class Facebook : MonoBehaviour
+    {
+
+        // Awake function from Unity's MonoBehavior
+        void Awake()
+        {
+            if (!FB.IsInitialized)
+            {
+                // Initialize the Facebook SDK
+                FB.Init(InitCallback, OnHideUnity);
+            }
+            else
+            {
+                // Already initialized, signal an app activation App Event
+                FB.ActivateApp();
+            }
+        }
+
+
+        void InitCallback()
+        {
+            if (FB.IsInitialized)
+            {
+                // Signal an app activation App Event
+                FB.ActivateApp();
+            }
+            else
+            {
+                Debug.Log("Failed to Initialize the Facebook SDK");
+            }
+        }
+
+        void OnHideUnity(bool isGameShown)
+        {
+            if (!isGameShown)
+            {
+                // Pause the game - we will need to hide
+                Time.timeScale = 0;
+            }
+            else
+            {
+                // Resume the game - we're getting focus again
+                Time.timeScale = 1;
+            }
+        }
+
+        public void FacebookLogin()
+        {
+            var perms = new List<string>() { "public_profile", "email" };
+            FB.LogInWithReadPermissions(perms, AuthCallback);
+        }
+
+
+        //Login
+        void AuthCallback(ILoginResult result)
+        {
+            if (FB.IsLoggedIn)
+            {
+                // AccessToken class will have session details
+                var aToken = AccessToken.CurrentAccessToken;
+                // Print current access token's User ID
+                Debug.Log(aToken.UserId);
+                // Print current access token's granted permissions
+                foreach (string perm in aToken.Permissions)
+                {
+                    Debug.Log(perm);
+                }
+            }
+            else
+            {
+                Debug.Log("User cancelled login");
+            }
+        }
+    }
+}
