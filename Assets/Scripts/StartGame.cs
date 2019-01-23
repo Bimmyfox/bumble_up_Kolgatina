5a7a2447bdb213074c66f7430f828506d97a9c74
bimmyfox <kolgatinadasha@gmail.com>
2019-01-23 06:15:23 +0300

    add start screen

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game
+{
+    public class StartGame : MonoBehaviour
+    {
+
+        void Update()
+        {
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+                SceneManager.LoadScene("Main", LoadSceneMode.Single);
+#endif
+            if (Input.touchCount > 0)
+                SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+        }
+    }
+}
